e7ad3a66952a3a59a1a88bd04e78e883e6fb0a33
Vijay G <vijay@Macbook.pro>
2020-07-25 18:15:21 +0100

    0.1: initial base version.

---
+++ androidapp/app/src/main/java/com/vj/app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.vj.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vj.app.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
